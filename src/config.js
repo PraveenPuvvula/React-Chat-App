e97584ab9f11d289ce6af81d165c3254ef56af98
Praveen Puvvula <praveenpuvvula43@gmail.com>
2018-10-05 03:37:23 -0400

    Final

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/5390bf87-10ce-426b-9926-4b3bb1e38064/token";
+
+const instanceLocator = "v1:us1:5390bf87-10ce-426b-9926-4b3bb1e38064";
+
+export { tokenUrl, instanceLocator }
+
+
+
